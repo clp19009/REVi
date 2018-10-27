1890ebbb33eeb374c647135b05765e2b6552791e
Naito Yusei <yusei.vitz.787@gmail.com>
2018-10-27 17:41:55 +0900

    2チームに対応

--- koyoscore.js
+++ koyoscore.js
@@ -2,8 +2,11 @@ var http = require('http');
 var socketio = require('socket.io');
 var fs = require('fs');
 
-var score = 0;
-var scores = [0];
+var score_a = 0;
+var score_b = 0;
+var scores_a = [0];
+var scores_b = [0];
+const scores_len_max = 100;
 
 var server = http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -22,26 +25,60 @@ io.sockets.on('connection', function (socket) {
         socket.join(room);
     });
 
-    socket.on('client_to_server', function (data) {
-        if (data.value != "init") {
-            if (data.value == "reset") {
-                score = 0;
-                scores.push(score);
-            } else if (data.value == "back") {
-                if (scores.length > 3) {
-                    score = scores[scores.length - 2];
-                    scores.pop();
+    socket.on('team_all', function (data) {
+        switch (data.value) {
+            case "init":
+                break;
+            case "back":
+                if (scores_a.length > 3) {
+                    score_a = scores_a[scores_a.length - 2];
+                    scores_a.pop();
                 }
-            } else {
-                score += data.value;
-                scores.push(score);
-            }
-            if (scores.length > 100) scores.shift();
+                if (scores_b.length > 3) {
+                    score_b = scores_b[scores_b.length - 2];
+                    scores_b.pop();
+                }
+                break;
+            case "reset":
+                score_a = 0;
+                scores_a.push(score_a);
+                score_b = 0;
+                scores_b.push(score_b);
+                break;
+            default:
         }
+        if (scores_a.length > scores_len_max) scores_a.shift();
+        if (scores_b.length > scores_len_max) scores_b.shift();
 
-        io.to(room).emit('server_to_client', { value: score });
+        io.to(room).emit('score', { score_a: score_a, score_b: score_b });
 
         // debug
-        console.log("score: " + score + "  input:" + data.value);
+        console.log("score A: " + score_a + " B: " + score_b + "  input: " + data.value);
+    });
+
+    socket.on('team_a', function (data) {
+        score_a += data.value;
+        scores_a.push(score_a);
+        scores_b.push(score_b);
+        if (scores_a.length > scores_len_max) scores_a.shift();
+        if (scores_b.length > scores_len_max) scores_b.shift();
+
+        io.to(room).emit('score', { score_a: score_a, score_b: score_b });
+
+        // debug
+        console.log("score A: " + score_a + " B: " + score_b + "  input: " + data.value);
+    });
+
+    socket.on('team_b', function (data) {
+        score_b += data.value;
+        scores_b.push(score_b);
+        scores_a.push(score_a);
+        if (scores_a.length > scores_len_max) scores_a.shift();
+        if (scores_b.length > scores_len_max) scores_b.shift();
+
+        io.to(room).emit('score', { score_a: score_a, score_b: score_b });
+
+        // debug
+        console.log("score A: " + score_a + " B: " + score_b + "  input: " + data.value);
     });
 });
